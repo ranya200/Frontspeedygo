e48d889e95dc59935b2aad031d25091afb0d62e1
Youssef <youssef.rahali@esprit.tn>
2025-02-16 18:27:52 +0100

    Save changes to Angular 19

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,12 +2,11 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink],
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'] ,
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    selector: 'app-dashboard',
+    imports: [RouterOutlet, RouterLink],
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css'],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DashboardComponent {
 

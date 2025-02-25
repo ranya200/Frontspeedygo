2892d1c1426c7480ca9d344482bd6e9196f01650
Houssem-tn <r.houssem123@gmail.com>
2025-02-25 23:13:55 +0100

    integ

--- src/app/FrontOffice/header-front/header-front.component.ts
+++ src/app/FrontOffice/header-front/header-front.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true, // ✅ Make it standalone
   imports: [CommonModule],
   templateUrl: './header-front.component.html',
-  styleUrls: ['./header-front.component.css']
+  styleUrls: ['./header-front.component.css',]
 })
 export class HeaderFrontComponent {
 
